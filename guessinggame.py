cca6982b3cbe486bc4ab42a60de8bdeff58087d7
Scott Cassidy <scassidy@gmail.com>
2014-11-17 11:53:48 -0800

    built out the game.
    Added functions: guess, correct, and again.

--- guessinggame.py
+++ guessinggame.py
@@ -1,31 +1,49 @@
 import random
-
 cpuNum = random.randint(1, 10)
-
+print cpuNum
 userNum = int(raw_input("Enter a number between 1 and 10: "))
-
 guessList = []
 
-while cpuNum != userNum:
+
+def guess(userNum):
 	if cpuNum == userNum:
-		pass
+		correct(guessList)
 
-	elif cpuNum < userNum:
-		print "Your guess is too high! Guess again"
-		userNum = int(raw_input("Enter another guess: "))
-		guessList.append(userNum)
+	elif cpuNum != userNum:
+		while cpuNum != userNum:
 
-	elif cpuNum > userNum:
-		print "Your guess is too low! Guess again"
-		userNum = int(raw_input("Enter another guess: "))
-		guessList.append(userNum)
+			if cpuNum < userNum:
+				print "Your guess is too high! Guess again"
+				userNum = int(raw_input("Enter another guess: "))
+				guessList.append(userNum)
+				return correct(guessList)
 
+			elif cpuNum > userNum:
+				print "Your guess is too low! Guess again"
+				userNum = int(raw_input("Enter another guess: "))
+				guessList.append(userNum)
+				return correct(guessList)
+
+			else:
+				print "Hmm, that guess returned an error."
+	
+	
+
+def correct(guessList):
+	guessListcount = len(guessList)
+
+	print "Yay! You guessed correctly!"
+	print "You made %d incorrect guesses before guessing correctly! Not bad!" % guessListcount
+	again()
+	
+def again():
+	again = raw_input("Would you like to play again? y/n?:  ")
+	if again == "y":
+		guess()
 	else:
-		print "Hmm, that guess returned an error."
+		print "Thanks for playing!"	
+
+guess(userNum)
 
 
-print "Yay! You guessed correctly!"
-guessListcount = len(guessList)
-print "You made %d incorrect guesses before guessing correctly! Not bad!" % guessListcount
-print cpuNum
-print userNum
+
